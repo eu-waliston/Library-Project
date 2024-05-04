40f6e0138f17d06a6b4af5adacda21d898c2b067
Waliston <wesantosdev@gmail.com>
2024-05-04 14:27:13 -0300

    update

--- library-back/src/API.js
+++ library-back/src/API.js
@@ -14,14 +14,21 @@ API.use(express.urlencoded({ extended: true }));
 API.use(cors());
 API.use(helmet());
 
-//Routes
+//Other Routes
+const RootRouter = require("../public/views/API.ejs");
 const UserRouter = require("./View/User.View");
 const BookRouter = require("./View/Book.View");
 const DOC = require("./View/Doc.View");
 
+API.use("/", RootRouter);
 API.use("/", UserRouter);
 API.use("/", BookRouter);
 API.use("/", DOC);
+
+//EJS
+API.use(express.static(__dirname + ".." + "/public"))
+API.use("views", path.join(__dirname, ".." + "/public/views"))
+API.set("view engine", "ejs")
 
 API.listen(process.env.PORT, () => {
     console.log(`API listening on PORT ${process.env.PORT}`);
