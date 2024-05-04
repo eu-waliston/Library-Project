dff622a510ad4f16b54f2c96f5bd24ac5f58ef2c
Waliston <wesantosdev@gmail.com>
2024-05-04 16:19:02 -0300

    home api page and docs page

--- library-back/src/API.js
+++ library-back/src/API.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-
+const path = require("path")
 require("dotenv").config();
 
 const API = express();
@@ -14,21 +14,20 @@ API.use(express.urlencoded({ extended: true }));
 API.use(cors());
 API.use(helmet());
 
-//Other Routes
-const RootRouter = require("../public/views/API.ejs");
+//EJS
+API.use(express.static(__dirname + ".." + 'public'));
+API.set('views', path.join(__dirname + ".." + 'public/views'))
+API.set('view engine', 'ejs');
+
+//Routes
+const RootRouter = require("./View/Root.View");
 const UserRouter = require("./View/User.View");
 const BookRouter = require("./View/Book.View");
 const DOC = require("./View/Doc.View");
-
 API.use("/", RootRouter);
 API.use("/", UserRouter);
 API.use("/", BookRouter);
 API.use("/", DOC);
-
-//EJS
-API.use(express.static(__dirname + ".." + "/public"))
-API.use("views", path.join(__dirname, ".." + "/public/views"))
-API.set("view engine", "ejs")
 
 API.listen(process.env.PORT, () => {
     console.log(`API listening on PORT ${process.env.PORT}`);
